c5978b839c20767bb7b8a8835187c212d9da1bde
RajviDave <rajvidave22@gmail.com>
2026-02-26 22:09:06 +0530

    calculated rms

--- vis.py
+++ vis.py
@@ -3,9 +3,12 @@ import matplotlib.pyplot as plt
 import re
 import datetime
 import statistics 
+import numpy as np
 
 date_time=[]
 values=[]
+window =32
+rms_values=[]
 
 with open('Data/AP01/Flow - 30-05-2024.txt','r') as f:
     lines=f.readlines()[7:]
@@ -18,5 +21,13 @@ df=pd.DataFrame({'Date_Time':date_time,'Values':values})
 df['Date_Time']=pd.to_datetime(df['Date_Time'], format="%d.%m.%Y %H:%M:%S,%f")
 df['Date_Time']=df['Date_Time'].dt.floor("s")
 
-for i in range(32):
-    print(df['Values'][i])
+df['Values']=pd.to_numeric(df['Values'])
+for i in range(0,len(df['Values']),window):
+    piece=df['Values'][i:i+window]
+    
+    if len(piece)== window:
+        rms = np.sqrt(np.mean(np.square(piece)))
+        rms_values.append(rms)
+
+print(len(rms_values))
+    
