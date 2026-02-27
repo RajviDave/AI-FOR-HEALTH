892d59893a0cf94d233fdbdd700a1d51d1c745f0
RajviDave <rajvidave22@gmail.com>
2026-02-27 19:11:27 +0530

    perfect values

--- vis.py
+++ vis.py
@@ -1,8 +1,17 @@
 import pandas as pd
 import re
+import datetime
 
-with open('Data/AP01/Flow - 30-05-2024.txt','r') as file:
-    lines = file.readlines()[7:]
+Date_time=[]
+Value=[]
+
+with open('Data/AP01/Flow - 30-05-2024.txt') as file:
+    lines=file.readlines()[7:]
     for line in lines:
-        line=line.strip()
-        data=line.split(';')
+        lines=line.strip()
+        data=lines.split(';')
+        Date_time.append(data[0])
+        Value.append(data[1])
+    
+    print(Value[:1000])
+
