f8c2dbe0e2507323c78abe39f822874a99d05def
RajviDave <rajvidave22@gmail.com>
2026-03-03 15:32:28 +0530

    pdf generated

--- plotting.py
+++ plotting.py
@@ -1,4 +1,6 @@
+import os
 import matplotlib.pyplot as plt
+from matplotlib.backends.backend_pdf import PdfPages
 from vis_flow import t_flow, rms_values
 from vis_thorac import t_thorac, mean_values
 from vis_spo2 import t_spo2, median_value
@@ -22,10 +24,7 @@ mask_air = (t_flow >= start_time) & (t_flow <= end_time)
 t_air_trim = t_flow[mask_air]
 airflow_trim = airflow[mask_air]
 
-print(mask_air)
-print(airflow_trim)
-
-# # Trim thoracic
+# Trim thoracic
 mask_th = (t_thorac >= start_time) & (t_thorac <= end_time)
 t_th_trim = t_thorac[mask_th]
 thoracic_trim = thoracic[mask_th]
@@ -35,29 +34,52 @@ mask_spo2 = (t_spo2 >= start_time) & (t_spo2 <= end_time)
 t_spo2_trim = t_spo2[mask_spo2]
 spo2_trim = spo2[mask_spo2]
 
-# Only interpolate if SpO2 has data
+# Interpolate SpO2 if possible
 if len(t_spo2_trim) > 1 and len(t_air_trim) > 1:
     t_air_sec = np.array([(t - start_time).total_seconds() for t in t_air_trim])
     t_spo2_sec = np.array([(t - start_time).total_seconds() for t in t_spo2_trim])
     spo2_resampled = np.interp(t_air_sec, t_spo2_sec, spo2_trim)
 else:
-    spo2_resampled = []
+    spo2_resampled = np.zeros(len(t_air_trim))
 
-# Plot
-fig, axes = plt.subplots(3, 1, figsize=(15, 8), sharex=True)
+# ---------------------------
+# Downsample to 5-second interval
+# ---------------------------
+t_air_sec = np.array([(t - t_air_trim[0]).total_seconds() for t in t_air_trim])
 
-axes[0].plot(t_air_trim, airflow_trim)
-axes[0].set_ylabel("Airflow")
+indices_5s = np.where(t_air_sec % 5 < 0.1)[0]
 
-axes[1].plot(t_th_trim, thoracic_trim)
-axes[1].set_ylabel("Thoracic")
+t_5s = t_air_trim[indices_5s]
+air_5s = airflow_trim[indices_5s]
+thor_5s = thoracic_trim[indices_5s]
+spo2_5s = spo2_resampled[indices_5s]
 
-if len(spo2_resampled) > 0:
-    axes[2].plot(t_air_trim, spo2_resampled)
-else:
-    axes[2].plot(t_spo2_trim, spo2_trim)
+# ---------------------------
+# Create PDF with chunked plots
+# ---------------------------
+pdf_path = "Visualizations/sleep_visualization.pdf"
 
-axes[2].set_ylabel("SpO2")
+chunk_size = 45
+total_points = len(t_5s)
 
-plt.tight_layout()
-plt.show()
+with PdfPages(pdf_path) as pdf:
+    for i in range(0, total_points, chunk_size):
+        end = i + chunk_size
+
+        fig, axes = plt.subplots(3, 1, figsize=(12, 8), sharex=True)
+
+        axes[0].plot(t_5s[i:end], air_5s[i:end])
+        axes[0].set_ylabel("Airflow")
+
+        axes[1].plot(t_5s[i:end], thor_5s[i:end])
+        axes[1].set_ylabel("Thoracic")
+
+        axes[2].plot(t_5s[i:end], spo2_5s[i:end])
+        axes[2].set_ylabel("SpO2")
+        axes[2].set_xlabel("Time")
+
+        plt.tight_layout()
+        pdf.savefig(fig)
+        plt.close(fig)
+
+print("PDF saved at:", pdf_path)
