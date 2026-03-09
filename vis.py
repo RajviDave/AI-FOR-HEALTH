3f8a4bf7ebc392e0d83c71b60d571351329c0eab
RajviDave <rajvidave22@gmail.com>
2026-03-09 12:19:32 +0530

    argument passing done

--- vis.py
+++ vis.py
@@ -3,13 +3,14 @@ import os
 import matplotlib.pyplot as plt
 
 from vis_flow import vis_flow
-from vis_thorac import vis_thorac
+from vis_thorac import thorac
 from vis_spo2 import vis_spo2
 from vis_eventflow import vis_eventflow
 
 
+# command line argument
 parser = argparse.ArgumentParser()
-parser.add_argument("-name", required=True, help="Participant folder path")
+parser.add_argument("-name", required=True, help="Participant folder")
 
 args = parser.parse_args()
 
@@ -17,50 +18,51 @@ folder = args.name
 participant = os.path.basename(folder)
 
 
-# call your existing scripts
-flow_time, flow_val = vis_flow(folder)
-thor_time, thor_val = vis_thorac(folder)
-spo2_time, spo2_val = vis_spo2(folder)
-events = vis_eventflow(folder)
+# get processed signals
+t_flow, flow_values = vis_flow(folder,"flow.txt")
+t_thorac, thorac_values = thorac(folder,"thorac.txt")
+t_spo2, spo2_values = vis_spo2(folder,"spo2.txt")
+
+# events = vis_eventflow(folder)
 
 
-# create stacked plot
+# create stacked plots
 fig, axes = plt.subplots(3, 1, figsize=(15,10), sharex=True)
 
 
-# Nasal airflow
-axes[0].plot(flow_time, flow_val)
+# airflow
+axes[0].plot(t_flow, flow_values)
 axes[0].set_title("Nasal Airflow")
 
 
-# Thoracic movement
-axes[1].plot(thor_time, thor_val)
+# thoracic movement
+axes[1].plot(t_thorac, thorac_values)
 axes[1].set_title("Thoracic Movement")
 
 
-# SpO2
-axes[2].plot(spo2_time, spo2_val)
+# spo2
+axes[2].plot(t_spo2, spo2_values)
 axes[2].set_title("SpO2")
 
 
-# overlay breathing events
-for event in events:
+# overlay events
+# for event in events:
 
-    start = event["start"]
-    end = event["end"]
+#     start = event["start"]
+#     end = event["end"]
 
-    for ax in axes:
-        ax.axvspan(start, end, alpha=0.3)
-
-
-plt.xlabel("Time")
+#     for ax in axes:
+#         ax.axvspan(start, end, alpha=0.3)
 
 
-# create visualization folder
+# plt.xlabel("Time")
+
+
+# create output folder
 os.makedirs("Visualizations", exist_ok=True)
 
-output = f"Visualizations/{participant}_visualization.pdf"
+output_path = f"Visualizations/{participant}_visualization.pdf"
 
-plt.savefig(output)
+plt.savefig(output_path)
 
-print("Visualization saved:", output)
+print("Saved:", output_path)
